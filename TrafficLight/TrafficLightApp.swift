3bb008c0ddca8e35368dcccb39dd18cbdd7335f7
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-06-09 21:22:25 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Alexey Kanaev on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
