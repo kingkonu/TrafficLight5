f92de1bba6881bd4a5cd9141ac4db4b1e6a32e96
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-06-11 22:31:11 +0300

    test

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
         if buttonText == "Start"{
             buttonText = "Next"
         }
-
+//вв
         switch currentLight {
         case .red:
             greenLight = lightIsOff
